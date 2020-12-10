9b140ce52a5187ff7e8f8322e92356e0b02d75b7
Sameer9995 <73990147+Sameer9995@users.noreply.github.com>
2020-12-10 17:35:20 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+     float a,b;
+     clrscr();
+     cout<<"Enter two number";
+     cin>>a>>b;
+     a=a+b;
+     cout<<a;
+getch();
+}     
